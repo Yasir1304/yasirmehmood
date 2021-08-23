6d54a5d79db80e88ed595fdaed9a086607e556e2
Yasir1304 <89351020+Yasir1304@users.noreply.github.com>
2021-08-23 23:58:54 +0530

    commit

--- demoproject/settings.py
+++ demoproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h+n_+g4q53=j(cfu=zs($)e1o2@6acxwvjx-)^%r!$(eakl(42
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','']
+ALLOWED_HOSTS = ['127.0.0.1','yasirmehmood.herokuapp.com/']
 
 
 # Application definition
